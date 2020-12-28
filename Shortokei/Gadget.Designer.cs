a58717e8f75445706b48c16ec1761ebfc91cd73a
Ponta <ponta@ponpon.com>
2020-12-29 02:11:11 +0900

    アイコン調整

--- Shortokei/Gadget.Designer.cs
+++ Shortokei/Gadget.Designer.cs
@@ -1,33 +1,33 @@
 ﻿namespace Shortokei
 {
-	partial class Gadget
-	{
-		/// <summary>
-		/// 必要なデザイナー変数です。
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Gadget
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// 使用中のリソースをすべてクリーンアップします。
-		/// </summary>
-		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows フォーム デザイナーで生成されたコード
+        #region Windows フォーム デザイナーで生成されたコード
 
-		/// <summary>
-		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
-		/// コード エディターで変更しないでください。
-		/// </summary>
-		private void InitializeComponent()
-		{
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gadget));
             this.Timer_Update = new System.Windows.Forms.Timer(this.components);
@@ -113,7 +113,7 @@
             // NotifyIcon_Main
             // 
             this.NotifyIcon_Main.ContextMenuStrip = this.ContextMenuStrip_Main;
-            this.NotifyIcon_Main.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyIcon_Main.Icon")));
+            this.NotifyIcon_Main.Icon = Properties.Resources.tokei;
             this.NotifyIcon_Main.Text = "しょーとけい";
             this.NotifyIcon_Main.Visible = true;
             this.NotifyIcon_Main.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_Main_MouseClick);
@@ -125,7 +125,7 @@
             this.ContextMenuStrip_Main.ResumeLayout(false);
             this.ResumeLayout(false);
 
-		}
+        }
 
         #endregion
 
